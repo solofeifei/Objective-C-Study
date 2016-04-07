27a649d6c2e897286c551636bfc01bb38ed1bec3
dengff <dengff@tech.youyuan.com>
2016-04-08 00:28:44 +0800

    first oc program

---
+++ Hello-OC/Hello-OC/main.m
@@ -0,0 +1,13 @@
+//第一个OC程序
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
